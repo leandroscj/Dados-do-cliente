113fab00efbcc44c3beb06bc5fe40f82e29c0e0a
Leandro Sousa <leandroscjTI@gmail.com>
2020-07-09 09:58:03 -0300

    Add project files.

---
+++ Dados do cliente/Cliente.cs
@@ -0,0 +1,31 @@
+﻿namespace Dados_do_cliente
+{
+    class Cliente
+    {
+        public int NumeroConta { get; private set; }
+        public string Nome { get; private set; }
+        public double Saldo { get; private set; }
+
+        public Cliente(string nomeC, int numeroConta, double saldoC)
+        {
+            NumeroConta = numeroConta;
+            Nome = nomeC;
+            Saldo = saldoC;
+        }
+
+        public void Deposito(double deposito)
+        {
+            Saldo += deposito;
+        }
+        public void Saque(double saque)
+        {
+            Saldo -= saque + 5; 
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + NumeroConta + ", Titular: " + Nome + ", Saldo: " + Saldo.ToString("F2");
+        }
+    }
+    
+}
---
+++ Dados do cliente/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Dados_do_cliente
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite o numero da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do titular da conta :");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito na conta? (s/n) ");
+            string afirmacao = Console.ReadLine();
+
+            if (afirmacao == "s")
+            {
+                Console.Write("Digite o valor do depósito: ");
+            }
+
+            double deposito = double.Parse(Console.ReadLine());
+
+            Cliente c = new Cliente(nome, conta, deposito);
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c);
+
+            Console.Write("Digite um valor para depósito: ");
+            double depositoc = double.Parse(Console.ReadLine());
+            c.Deposito(depositoc);
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c);
+
+
+            Console.Write("Digite um valor para saque: ");
+            double saquec = double.Parse(Console.ReadLine());
+            c.Saque(saquec);
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(c);
+
+
+
+        }
+    }
+}
